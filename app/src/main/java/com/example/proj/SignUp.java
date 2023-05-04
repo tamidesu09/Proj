ff2d5d50b0ec1dcf3e1a4659570ad3336c9fd45a
Jenny Araneta <mjmaraneta@tip.edu.ph>
2023-05-04 12:25:51 +0700

    Firebase and Firebase Authentication implemented, as well as added the google-services.json

--- app/src/main/java/com/example/proj/SignUp.java
+++ app/src/main/java/com/example/proj/SignUp.java
@@ -1,15 +1,31 @@
 package com.example.proj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
@@ -19,6 +35,9 @@ public class SignUp extends AppCompatActivity {
     TextView mloginBtn;
     ProgressBar progressBar;
 
+    FirebaseFirestore fstore;
+    String userID;
+    FirebaseAuth fAuth;
 
 
     @Override
@@ -33,6 +52,11 @@ public class SignUp extends AppCompatActivity {
         mSignupBtn = findViewById(R.id.SignupBtn);
         mloginBtn = findViewById(R.id.SignupBtn);
         progressBar = findViewById(R.id.progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+
 
         mloginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +76,6 @@ public class SignUp extends AppCompatActivity {
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required");
                     return;
-
                 }
 
                 if(TextUtils.isEmpty(password)){
@@ -66,6 +89,60 @@ public class SignUp extends AppCompatActivity {
                 }
 
                 progressBar.setVisibility(view.VISIBLE);
+
+
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                         FirebaseUser fuser= fAuth.getCurrentUser();
+                         fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                             @Override
+                             public void onSuccess(Void unused) {
+                                 Toast.makeText(getApplicationContext(), "Signed Up Successfully", Toast.LENGTH_SHORT)
+                             }
+                         }).addOnFailureListener(new OnFailureListener() {
+                             @Override
+                             public void onFailure(@NonNull Exception e) {
+                                 Log.d(TAG, "Onfailure: Email Not Sent" + e.getMessage());
+                             }
+                         });
+
+                            Toast.makeText(getApplicationContext(), "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("user").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName", fullName);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "onSuccess: User Profile is Created for " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        }
+
+                        else {
+
+                            Toast.makeText(SignUp.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+
             }
         });
 
