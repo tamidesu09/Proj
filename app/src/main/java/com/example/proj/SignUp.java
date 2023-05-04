535319839d0a75068830695d8e73f48deaa9a610
Jenny Araneta <mjmaraneta@tip.edu.ph>
2023-05-04 11:35:30 +0700

    Added functions to the buttons such as OnClick

--- app/src/main/java/com/example/proj/SignUp.java
+++ app/src/main/java/com/example/proj/SignUp.java
@@ -2,13 +2,73 @@ package com.example.proj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mSignupBtn;
+    TextView mloginBtn;
+    ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mFullName = findViewById(R.id.FullName);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.Password);
+        mPhone = findViewById(R.id.Phone);
+        mSignupBtn = findViewById(R.id.SignupBtn);
+        mloginBtn = findViewById(R.id.SignupBtn);
+        progressBar = findViewById(R.id.progressBar);
+
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+
+        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String phone = mPhone.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+                if (password.length() < 6){
+                    mPassword.setError("Password must be >= 6 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(view.VISIBLE);
+            }
+        });
+
+
     }
 }
